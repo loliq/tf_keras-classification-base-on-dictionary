f4b7bf5535ec32678bc88faa8d15996034cb8831
loliq <445780564@qq.com>
2019-11-01 20:39:39 +0800

    Commit on 2019 11/1 Define DenseNet and ResNet

---
+++ models/DenseNet.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+tf.enable_eager_execution()
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D,Dropout
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.models import Model, load_model
+
+
+
+# In[ ]:
+
+
+global_regulizer = keras.regularizers.l2(0.00001)
+
+
+# ###  1. 定义卷积层
+
+# In[17]:
+
+
+def conv_block(X, stage, branch, nb_filter, dropout_rate=None, weight_decay=1e-4):
+    '''Apply BatchNorm, Relu, bottleneck 1x1 Conv2D, 3x3 Conv2D, and option dropout
+        # Arguments
+            x: input tensor 
+            stage: index for dense block
+            branch: layer index within each dense block
+            nb_filter: number of filters
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+    '''
+    conv_name_base = 'conv' + str(stage) + '_' +'block'+ str(branch) + '_'+ '{0}'+ '_' + 'conv'
+    bn_name_base = 'conv' + str(stage) + '_' +'block'+ str(branch) + '_'+ '{0}'+ '_' + 'bn'
+    relu_name_base = 'conv' + str(stage) + '_' +'block'+ str(branch) + '_'+ '{0}'+ '_' + 'relu'
+
+    # 1x1 Convolution (Bottleneck layer)
+    inter_channel = nb_filter * 4  
+    X = BatchNormalization(axis = 3, name=bn_name_base.format(0))(X)
+    X = Activation('relu',name = relu_name_base.format(0))(X)
+    X = Conv2D(inter_channel, (1,1), name = conv_name_base.format(1),use_bias=False, kernel_regularizer=global_regulizer)(X)
+    ###
+    if dropout_rate:
+        X = Dropout(dropout_rate)(X)
+    # 3x3 Convolution
+    X = BatchNormalization(axis = 3, name=bn_name_base.format(1))(X)
+    X = Activation('relu',name = relu_name_base.format(1))(X)
+    X = Conv2D(nb_filter, (3,3), name = conv_name_base.format(2), padding='same', use_bias=False, kernel_regularizer=global_regulizer)(X)
+    
+    if dropout_rate:
+        X = Dropout(dropout_rate)(X)
+
+    return X
+
+
+# ###  2. 定义转换层
+
+# In[18]:
+
+
+def transition_block(X, stage, nb_filter, compression=1.0, dropout_rate=None, weight_decay=1E-4):
+    ''' Apply BatchNorm, 1x1 Convolution, averagePooling, optional compression, dropout 
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_filter: number of filters
+            compression: calculated as 1 - reduction. Reduces the number of feature maps in the transition block.
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+    '''
+
+    eps = 1.1e-5
+    conv_name_base = 'pool' + str(stage) + '_conv'
+    bn_name_base = 'pool' + str(stage) + '_bn'
+    relu_name_base = 'pool' + str(stage) + '_relu'
+    pool_name_base = 'pool' + str(stage) + '_pool'
+
+    X = BatchNormalization(axis = 3, name=bn_name_base)(X)
+    X = Activation('relu', name=relu_name_base)(X)
+    X = Conv2D(int(nb_filter * compression), (1,1), name = conv_name_base, use_bias=False, kernel_regularizer=global_regulizer)(X)
+    if dropout_rate:
+        X = Dropout(dropout_rate)(X)
+
+    X = AveragePooling2D((2, 2), strides=(2, 2), name=pool_name_base)(X)
+
+    return X
+
+
+# ###  3. 定义dense_block
+
+# In[19]:
+
+def se_block(x, stage,radio=4):
+    shape = x.get_shape().as_list()
+    channel_out = shape[3]
+    pool_name = 'SEblock_stage{0}_pool'.format(stage)
+    dense_name = 'SEblock_stage{0}_dense_{1}'
+    
+    x_radio = layers.GlobalAveragePooling2D(name = pool_name)(x)
+    x_radio = Flatten()(x_radio)
+    
+    x_radio = Dense(int(channel_out/radio),kernel_regularizer=global_regulizer,name = dense_name.format(stage,1))(x_radio)
+    x_radio = Dense(channel_out, kernel_regularizer=global_regulizer, name = dense_name.format(stage,2))(x_radio)
+    x_radio = tf.reshape(x_radio, [-1, 1, 1, channel_out])
+    
+    x_radio = x_radio * x 
+    return x_radio
+
+def dense_block(X, stage, nb_layers, nb_filter, growth_rate, dropout_rate=None, weight_decay=1e-4, grow_nb_filters=True):
+    ''' Build a dense_block where the output of each conv_block is fed to subsequent ones
+        # Arguments
+            x: input tensor
+            stage: index for dense block
+            nb_layers: the number of layers of conv_block to append to the model.
+            nb_filter: number of filters
+            growth_rate: growth rate
+            dropout_rate: dropout rate
+            weight_decay: weight decay factor
+            grow_nb_filters: flag to decide to allow number of filters to grow
+    '''
+
+    concat_feat = X
+
+    for i in range(nb_layers):
+        branch = i+1
+        x = conv_block(concat_feat, stage, branch, growth_rate, dropout_rate, weight_decay)
+        concat_name = 'conv{0}_block{1}_concat'.format(stage, branch)
+        concat_feat = keras.layers.concatenate(inputs=[concat_feat, x], axis=3, name=concat_name)
+        if grow_nb_filters:
+            nb_filter += growth_rate
+
+    return concat_feat, nb_filter
+
+
+# In[20]:
+
+
+def DenseNet121(nb_dense_block=4, growth_rate=32, nb_filter=64, reduction=0.0, dropout_rate=0.0, weight_decay=1e-4, classes=1000, weights_path=None):
+    
+    X_input = Input((224,224, 3))
+    X = X_input
+    compression = 1.0 - reduction
+    nb_dense_block = 4
+    nb_filter = 64
+    nb_layers = [6,12,24,16] # For DenseNet-121
+    # stage 1 
+    X = ZeroPadding2D((3,3))(X)
+    X = Conv2D(nb_filter, (7,7), name='conv1/conv',strides=(2,2),use_bias = False,kernel_regularizer=global_regulizer)(X)    
+    X = BatchNormalization(axis = 3, name = 'conv1/bn')(X)
+    X = Activation('relu', name = 'conv1/relu')(X)
+    
+    X = ZeroPadding2D((1,1))(X)
+    X = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding = 'valid',name='pool1')(X)
+    
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        stage = block_idx+2
+        X, nb_filter = dense_block(X, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        # Add transition_block
+        X = transition_block(X, stage, nb_filter, compression=compression, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        nb_filter = int(nb_filter * compression)
+    final_stage = stage + 1
+    X, nb_filter = dense_block(X, final_stage, nb_layers[-1], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+    
+    X = BatchNormalization(axis = 3, name='bn')(X)
+    X = Activation('relu', name='relu')(X)
+    X = keras.layers.GlobalAvgPool2D()(X)
+    X = Dense(classes, name = 'fc' + str(classes), activation=None,kernel_regularizer=global_regulizer)(X)
+    model = keras.Model(inputs = X_input, outputs = X, name = 'DenseNet121')
+    
+    if weights_path is not None:
+        model.load_weights(weights_path)
+    
+    return model
+
+
+# In[25]:
+
+def DenseNet_lighter(nb_dense_block=4, growth_rate=12, nb_filter=64, reduction=0.2, dropout_rate=0.2, weight_decay=1e-4, classes=1000, weights_path=None):
+    X_input = Input((224,224, 3))
+    X = X_input
+    compression = 1.0 - reduction
+    nb_dense_block = 4
+    nb_filter = nb_filter
+    nb_layers = [2,4,6,4] # For DenseNet-121
+    # stage 1 
+    X = ZeroPadding2D((3,3))(X)
+    X = Conv2D(nb_filter, (7,7), name='conv1/conv',strides=(2,2),use_bias = False,kernel_regularizer=global_regulizer)(X)    
+    X = BatchNormalization(axis = 3, name = 'conv1/bn')(X)
+    X = Activation('relu', name = 'conv1/relu')(X)
+    
+    X = ZeroPadding2D((1,1))(X)
+    X = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding = 'valid',name='pool1')(X)
+    
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        stage = block_idx+2
+        X, nb_filter = dense_block(X, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        # Add transition_block
+        X = transition_block(X, stage, nb_filter, compression=compression, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        nb_filter = int(nb_filter * compression)
+    final_stage = stage + 1
+    X, nb_filter = dense_block(X, final_stage, nb_layers[-1], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+    
+    X = BatchNormalization(axis = 3, name='bn')(X)
+    X = Activation('relu', name='relu')(X)
+    X = keras.layers.GlobalAvgPool2D()(X)
+    X = Dense(classes, name = 'fc' + str(classes), activation='softmax',kernel_regularizer=global_regulizer)(X)
+    model = keras.Model(inputs = X_input, outputs = X, name = 'DenseNet_lighter')
+    
+    if weights_path is not None:
+        model.load_weights(weights_path)
+    
+    return model
+
+def DenseNet_SE_lighter(nb_dense_block=4, growth_rate=12, nb_filter=64, reduction=0.2, dropout_rate=0.2, weight_decay=1e-4, classes=1000, weights_path=None):
+    X_input = Input((224,224, 3))
+    X = X_input
+    compression = 1.0 - reduction
+    nb_dense_block = 4
+    nb_filter = nb_filter
+    nb_layers = [2,4,6,4] # For DenseNet-121
+    # stage 1 
+    X = ZeroPadding2D((3,3))(X)
+    X = Conv2D(nb_filter, (7,7), name='conv1/conv',strides=(2,2),use_bias = False,kernel_regularizer=global_regulizer)(X)    
+    X = BatchNormalization(axis = 3, name = 'conv1/bn')(X)
+    X = Activation('relu', name = 'conv1/relu')(X)
+    
+    X = ZeroPadding2D((1,1))(X)
+    X = MaxPooling2D(pool_size=(3,3), strides=(2,2), padding = 'valid',name='pool1')(X)
+    
+    # Add dense blocks
+    for block_idx in range(nb_dense_block - 1):
+        stage = block_idx+2
+        X, nb_filter = dense_block(X, stage, nb_layers[block_idx], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        # Add transition_block
+        X = se_block(X, stage)
+        X = transition_block(X, stage, nb_filter, compression=compression, dropout_rate=dropout_rate, weight_decay=weight_decay)
+        nb_filter = int(nb_filter * compression)
+    final_stage = stage + 1
+    X, nb_filter = dense_block(X, final_stage, nb_layers[-1], nb_filter, growth_rate, dropout_rate=dropout_rate, weight_decay=weight_decay)
+    
+    X = BatchNormalization(axis = 3, name='bn')(X)
+    X = Activation('relu', name='relu')(X)
+    X = keras.layers.GlobalAvgPool2D()(X)
+    X = Dense(classes, name = 'fc' + str(classes), activation='sigmoid',kernel_regularizer=global_regulizer)(X)
+    model = keras.Model(inputs = X_input, outputs = X, name = 'DenseNet_lighter')
+    
+    if weights_path is not None:
+        model.load_weights(weights_path)
+    
+    return model
+
+
+# ###  4. 定义预处理函数
+
+# In[26]:
+
+
+if __name__ == '__main__':
+    model = DenseNet_lighter(reduction=0.5, classes = 2)
+    model.summary()
+
+
+# In[ ]:
+
+
+
+
---
+++ models/ResNet.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/31 15:05
+# @Author  : LLL
+# @Site    : 
+# @File    : ResNet.py
+# @Software: PyCharm
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras import Model
+
+
+# GRADED FUNCTION: identity_block
+
+def identity_block(X, f, filters, stage, block):
+    """
+    Implementation of the identity block as defined in Figure 4
+    res_net50 的identity blcok
+    适用于RestNet 50，101，152
+    conv 为[ 1 x1 , f1]
+            [3x3, f2]
+            [1x1, f3]
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    assert len(filters) == 3
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value. You'll need this later to add back to the main path.
+    X_shortcut = X
+
+    # First component of main path
+    X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    ### START CODE HERE ###
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = layers.add([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    ### END CODE HERE ###
+
+    return X
+
+
+# GRADED FUNCTION: convolutional_block
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+    """
+    Implementation of the convolutional block as defined in Figure 4
+
+    You can use this type of block when the input and output dimensions don't match up.
+    The difference with the identity block is that there is a CONV2D layer in the shortcut path:
+    作用是做下采样feature_map, 减小为原来的1/s, 还有就是不同的channel之间的变换
+    以[256,256,1024] X 6 为例 ，因为最后一层的输出为1024， 所以输入的channel 也需要是1024. 所以需要在第一个block 进行变换
+    后面的因为都是前面的输出为输入，所以没有channel维度匹配的问题
+    当输入输出维度不同的时候，可以用这个模块转换，与 identity layer 不同的是，在 shortcut_path 有一个额外一个conv2d,用于
+    输入feature_map的size转换
+    feature
+    其余与identity_block相同
+    main_path: [1x1, f1]
+                [3x3, f2]
+                [1x1,f3]
+    适用于RestNet 50，101，152
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+    assert len(filters) == 3
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value
+    X_shortcut = X
+
+    ##### MAIN PATH #####
+    # First component of main path
+    X = Conv2D(F1, (1, 1), strides=(s, s), name=conv_name_base + '2a', padding='valid',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    ### START CODE HERE ###
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(F2, (f, f), strides=(1, 1), name=conv_name_base + '2b', padding='same',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(F3, (1, 1), strides=(1, 1), name=conv_name_base + '2c', padding='valid',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    ##### SHORTCUT PATH #### (≈2 lines)
+    X_shortcut = Conv2D(F3, (1, 1), strides=(s, s), name=conv_name_base + '1', padding='valid',
+                        kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = layers.add([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    ### END CODE HERE ###
+
+    return X
+
+
+# GRADED FUNCTION: ResNet50
+
+def ResNet50(input_shape=(64, 64, 3), classes=6):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    # The convolutional block uses three set of filters of size [128,128,512], "f" is 3, "s" is 2 and the block is "a".
+    # The 3 identity blocks use three set of filters of size [128,128,512], "f" is 3 and the blocks are "b", "c" and "d".
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block='b')
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block='c')
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    # The convolutional block uses three set of filters of size [256, 256, 1024], "f" is 3, "s" is 2 and the block is "a".
+    # The 5 identity blocks use three set of filters of size [256, 256, 1024], "f" is 3 and the blocks are "b", "c", "d", "e" and "f".
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], block='a', stage=4, s=2)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], block='b', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], block='c', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], block='d', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], block='e', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], block='f', stage=4)
+
+    # Stage 5 (≈3 lines)
+    # The convolutional block uses three set of filters of size [512, 512, 2048], "f" is 3, "s" is 2 and the block is "a".
+    # The 2 identity blocks use three set of filters of size [256, 256, 2048], "f" is 3 and the blocks are "b" and "c".
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+
+    # filters should be [256, 256, 2048], but it fail to be graded. Use [512, 512, 2048] to pass the grading
+    X = identity_block(X, f=3, filters=[256, 256, 2048], stage=5, block='b')
+    X = identity_block(X, f=3, filters=[256, 256, 2048], stage=5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # The 2D Average Pooling uses a window of shape (2,2) and its name is "avg_pool".
+    X = AveragePooling2D(pool_size=(2, 2))(X)
+
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer=glorot_uniform(seed=0))(X)
+
+    # Create model
+    model = Model(inputs=X_input, outputs=X, name='ResNet50')
+
+    return model
+
+
+def identity_block_2(X, f, filters, stage, block):
+    """
+    适用于ResNet 18 和 34 的identity_block
+    :param X:
+    :param f:
+    :param filters:
+    :param stage:
+    :param block:
+    :return:
+    """
+    assert len(filters) == 2
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2 = filters
+
+    # Save the input value. You'll need this later to add back to the main path.
+    X_shortcut = X
+
+    # First component of main path
+    X = Conv2D(filters=F1, kernel_size=(f, f), strides=(1, 1), padding='valid', name=conv_name_base + '2a',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    # X = Activation('relu')(X)
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(F2, (f, f), strides=(1, 1), name=conv_name_base + '2b', padding='same',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = layers.add([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return  X
+
+def convolutional_block2(X, f, filters, stage, block, s=2):
+    """
+    适用于 resnet 18 和 resnet50 的convolutional_block
+    :param X:
+    :param f:
+    :param filters:
+    :param stage:
+    :param block:
+    :param s:
+    :return:
+    """
+    assert len(filters) == 2
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2 = filters
+
+    # Save the input value
+    X_shortcut = X
+
+    ##### MAIN PATH #####
+    # First component of main path
+    X = Conv2D(F1, (f, f), strides=(s, s), name=conv_name_base + '2a', padding='valid',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    ### START CODE HERE ###
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(F2, (f, f), strides=(1, 1), name=conv_name_base + '2b', padding='valid',
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+
+    ##### SHORTCUT PATH #### (≈2 lines)
+    X_shortcut = Conv2D(F2, (f, f), strides=(s, s), name=conv_name_base + '1', padding='valid',
+                        kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = layers.add([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    ### END CODE HERE ###
+
+    return X
+
+def ResNet_34(input_shape=(64, 64, 3), classes=6):
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f=3, filters=[64, 64], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64], stage=2, block='c')
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    # The convolutional block uses three set of filters of size [128,128,512], "f" is 3, "s" is 2 and the block is "a".
+    # The 3 identity blocks use three set of filters of size [128,128,512], "f" is 3 and the blocks are "b", "c" and "d".
+    X = convolutional_block(X, f=3, filters=[128, 128], stage=3, block='a', s=2)
+    X = identity_block(X, f=3, filters=[128, 128], stage=3, block='b')
+    X = identity_block(X, f=3, filters=[128, 128], stage=3, block='c')
+    X = identity_block(X, f=3, filters=[128, 128], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    # The convolutional block uses three set of filters of size [256, 256, 1024], "f" is 3, "s" is 2 and the block is "a".
+    # The 5 identity blocks use three set of filters of size [256, 256, 1024], "f" is 3 and the blocks are "b", "c", "d", "e" and "f".
+    X = convolutional_block(X, f=3, filters=[256, 256], block='a', stage=4, s=2)
+    X = identity_block(X, f=3, filters=[256, 256], block='b', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256], block='c', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256], block='d', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256], block='e', stage=4)
+    X = identity_block(X, f=3, filters=[256, 256], block='f', stage=4)
+
+    # Stage 5 (≈3 lines)
+    # The convolutional block uses three set of filters of size [512, 512, 2048], "f" is 3, "s" is 2 and the block is "a".
+    # The 2 identity blocks use three set of filters of size [256, 256, 2048], "f" is 3 and the blocks are "b" and "c".
+    X = convolutional_block(X, f=3, filters=[512, 512], stage=5, block='a', s=2)
+
+    # filters should be [256, 256, 2048], but it fail to be graded. Use [512, 512, 2048] to pass the grading
+    X = identity_block(X, f=3, filters=[512, 512], stage=5, block='b')
+    X = identity_block(X, f=3, filters=[512, 512], stage=5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # The 2D Average Pooling uses a window of shape (2,2) and its name is "avg_pool".
+    X = AveragePooling2D(pool_size=(2, 2))(X)
+
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer=glorot_uniform(seed=0))(X)
+
+    # Create model
+    model = Model(inputs=X_input, outputs=X, name='ResNet34')
+
+    return model
